8eb7d515e81bce88df062bf073bf4dfdc9336093
Aga <agajarosik@o2.pl>
2019-10-02 01:02:52 +0200

    Updates

---
+++ precache-manifest.b56a30cc33589bdaadd8bcb294ce1325.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed2d8cea2e3f25bf9b6f7695a0dbe6ae",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "3f99c799425e4822561e",
+    "url": "/markdown-previewer/static/css/main.213702b7.chunk.css"
+  },
+  {
+    "revision": "e0129bcd907d1567ccbb",
+    "url": "/markdown-previewer/static/js/2.6d6367cd.chunk.js"
+  },
+  {
+    "revision": "3f99c799425e4822561e",
+    "url": "/markdown-previewer/static/js/main.cc20ffc1.chunk.js"
+  },
+  {
+    "revision": "492a294d8783a83a94ad",
+    "url": "/markdown-previewer/static/js/runtime-main.7ab55704.js"
+  },
+  {
+    "revision": "47cf314e4ee706e6d1a74321cfe65078",
+    "url": "/markdown-previewer/static/media/fontello.47cf314e.ttf"
+  },
+  {
+    "revision": "554cb065f4194cc57b5ee0a18192d26d",
+    "url": "/markdown-previewer/static/media/fontello.554cb065.svg"
+  },
+  {
+    "revision": "6df27a5e295ecd02f6af8bd2503e3009",
+    "url": "/markdown-previewer/static/media/fontello.6df27a5e.woff2"
+  },
+  {
+    "revision": "9f8690b893a0214df92eceb7e56d8e32",
+    "url": "/markdown-previewer/static/media/fontello.9f8690b8.woff"
+  },
+  {
+    "revision": "d4a651895b5807b80c6e535c3e4e3fd2",
+    "url": "/markdown-previewer/static/media/fontello.d4a65189.eot"
+  }
+]);
